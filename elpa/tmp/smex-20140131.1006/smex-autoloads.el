746bc1196267c08ffe6fb65944fb64cf8ba15408
zyqhi <zyqhi2010@gmail.com>
2015-03-24 18:38:55 +0800

    delete useless files

--- elpa/tmp/smex-20140131.1006/smex-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; smex-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (21255
-;;;;;;  27773 690026 631000))
-;;; Generated autoloads from smex.el
-
-(autoload 'smex "smex" "\
-
-
-\(fn)" t nil)
-
-(autoload 'smex-initialize "smex" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("smex-pkg.el") (21255 27773 693557 597000))
-
-;;;***
-
-(provide 'smex-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; smex-autoloads.el ends here
--- vendor/useless/auto-complete/auto-complete-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; auto-complete-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (auto-complete) "auto-complete" "auto-complete.el"
-;;;;;;  (21219 11039 800910 159000))
-;;; Generated autoloads from auto-complete.el
-
-(autoload 'auto-complete "auto-complete" "\
-Start auto-completion at current point.
-
-\(fn &optional SOURCES)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el")
-;;;;;;  (21219 11039 805469 189000))
-
-;;;***
-
-(provide 'auto-complete-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; auto-complete-autoloads.el ends here
